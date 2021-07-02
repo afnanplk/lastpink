c0e88c3fdd29e9d8e77268974137fd29aa5cb871
Afnan PLK <83832372+afnanplk@users.noreply.github.com>
2021-07-02 23:25:21 +0530

    Update adan.js

--- plugins/adan.js
+++ plugins/adan.js
@@ -16,7 +16,7 @@ const butuh = "```give a city name!```"
 const renek = "```Not found the city!```"
 
 
-        Asena.addCommand({pattern: 'prayer ?(.*)', desc: adzan, usage: usage, fromMe: false}, async (message, match) => {
+        Asena.addCommand({pattern: 'adan ?(.*)', desc: adzan, usage: usage, fromMe: false}, async (message, match) => {
 
 	    if (match[1] === '') return await message.reply(butuh);
 	    const url = `https://api.pray.zone/v2/times/today.json?city=${match[1]}`;
