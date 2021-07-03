8c87277276001d8b043bd109f333023499a9348f
Afnan PLK <83832372+afnanplk@users.noreply.github.com>
2021-07-03 16:54:46 +0530

    Update QandA.js

--- plugins/QandA.js
+++ plugins/QandA.js
@@ -19,12 +19,12 @@ Asena.addCommand({ pattern: 'brainly ?(.*)', fromMe: false, desc: brainly, usage
           .get(`https://api.xteam.xyz/brainly?APIKEY=10c4105200edc0f0&soal=${Soal}`)
           .then(async (response) => {
             const {
-              question,
-              answer,
+              soal,
+              jawaban,
             } = response.data
 
-            const msg = `*Soal:* ${question}
-*Answer from Brainly:* ${answer.replace(/1Question/g, '*(1) Question*').replace(/2Question/g, '*(2) Question*').replace(/3Question/g, '*(3) Question*').replace(/Answer/g, '*Answer*').replace(/Brainly Found/g, '')}`
+            const msg = `*Soal:* ${soal}
+*Jawaban Brainly:* ${jawaban.replace(/1Question/g, '*(1) Question*').replace(/2Question/g, '*(2) Question*').replace(/3Question/g, '*(3) Question*').replace(/Answer/g, '*Answer*').replace(/Brainly Found/g, '')}`
             await message.client.sendMessage(message.jid, msg, MessageType.text)
            })
       },
