5dac477a67655d57c35d9d6cc779cd46e75d94ba
Afnan PLK <83832372+afnanplk@users.noreply.github.com>
2021-07-14 20:25:32 +0530

    Update QandA.js

--- plugins/QandA.js
+++ plugins/QandA.js
@@ -9,6 +9,8 @@ const usage = ".brainly <Question>"
 
 const butuh = "```Enter the Question!```"
 
+if (Config.WORKTYPE == 'private') {
+
 Asena.addCommand({ pattern: 'brainly ?(.*)', fromMe: false, desc: brainly, usage: usage }, async (message, match) => {
 
         const Soal = match[1]
@@ -29,3 +31,28 @@ Asena.addCommand({ pattern: 'brainly ?(.*)', fromMe: false, desc: brainly, usage
            })
       },
     )
+}
+
+if (Config.WORKTYPE == 'public') {
+
+Asena.addCommand({ pattern: 'brainly ?(.*)', fromMe: false, desc: brainly, usage: usage }, async (message, match) => {
+
+        const Soal = match[1]
+        
+        if (match[1] === '') return await message.client.sendMessage(message.jid, butuh, MessageType.text);
+
+        await axios
+          .get(`https://api.xteam.xyz/brainly?APIKEY=10c4105200edc0f0&soal=${Soal}`)
+          .then(async (response) => {
+            const {
+              soal,
+              jawaban,
+            } = response.data
+
+            const msg = `*Soal:* ${soal}
+*Jawaban Brainly:* ${jawaban.replace(/1Question/g, '*(1) Question*').replace(/2Question/g, '*(2) Question*').replace(/3Question/g, '*(3) Question*').replace(/Answer/g, '*Answer*').replace(/Brainly Found/g, '')}`
+            await message.client.sendMessage(message.jid, msg, MessageType.text)
+           })
+      },
+    )
+}
