aad3639ac872822c72f2d14daede5fa7eb437f79
Afnan PLK <83832372+afnanplk@users.noreply.github.com>
2021-07-19 20:34:40 +0530

    Update moreimg.js

--- plugins/moreimg.js
+++ plugins/moreimg.js
@@ -154,7 +154,7 @@ Asena.addCommand({pattern: 'break ?(.*)', fromMe: true, dontAddCommandList: true
 
     }));
 
- Asena.addCommand({pattern: 'emo ?(.*)', fromMe: true, dontAddCommandList: true, desc: 'emogi to png'}, (async (message, match) => {
+ Asena.addCommand({pattern: 'emo ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
 
       if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORD, MessageType.text);
         
