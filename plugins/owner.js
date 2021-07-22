52b5559782e49cfacf012426881057547289b309
Afnan PLK <83832372+afnanplk@users.noreply.github.com>
2021-07-22 16:48:28 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -1,8 +1,7 @@
-/* Copyright (C) 2020 TOXIC DEVIL
-CODDED BY TOXIC DEVIL
+/* Copyright (C) 2021 AFNAN PALLIKKEL
+CODDED AFNAN PALLIKKEL
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-WhatsAsenaPublic - TOXIC DEVIL
 */
 
 const Asena = require('../events');
