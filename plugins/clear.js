7e2f785262ee4011118fcabf04872643caeea7fc
Afnan PLK <83832372+afnanplk@users.noreply.github.com>
2021-07-25 09:54:00 +0530

    Update clear.js

--- plugins/clear.js
+++ plugins/clear.js
@@ -1,3 +1,9 @@
+/* Copyright (C) 2020 afnanplk.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+*/
+
+
 const {MessageType, GroupSettingChange, ChatModification, WAConnectionTest} = require('@adiwajshing/baileys');
 const Asena = require('../events');
 const Config = require('../config');
