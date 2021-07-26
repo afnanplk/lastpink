56cffcb79b1b8368dbc77917ca932dd940fafbe0
Afnan PLK <83832372+afnanplk@users.noreply.github.com>
2021-07-26 15:56:46 +0530

    Update anime.js

--- plugins/anime.js
+++ plugins/anime.js
@@ -188,7 +188,7 @@ else if (Config.WORKTYPE == 'public') {
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'PLK'})
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: Config.AFN})
 
     }));
 }
