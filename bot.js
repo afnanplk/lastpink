233b506bda6795f962d451c7b913643a2315e5e6
Afnan PLK <83832372+afnanplk@users.noreply.github.com>
2021-07-27 13:25:15 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -135,7 +135,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please wait.')}`);
         });
 
         console.log(
-            chalk.green.bold('✅ Plugins installed!')
+            chalk.green.bold('Pinky bot working perfectly 👻')
         );
     });
     
