64fefc8b6dd0e1734142e76f5425cd4fac3dc7ba
Afnan PLK <83832372+afnanplk@users.noreply.github.com>
2021-08-02 13:39:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 /* Copyright (C) 2020 Yusuf Usta.
-re edited by afnanplk  
+re edited by afnan-plk  
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
