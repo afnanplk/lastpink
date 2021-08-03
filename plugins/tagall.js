93415c481269d1152552e35c417ed562b937127f
Afnan PLK <83832372+afnanplk@users.noreply.github.com>
2021-08-03 14:06:07 +0530

    Update tagall.js

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -41,7 +41,7 @@ Asena.addCommand({pattern: 'tagall ?(.*)', fromMe: true, dontAddCommandList: tru
             mesaj = '';
             grup['participants'].map(
                 async (uye) => {
-                    mesaj += '\n 🙍🏻‍♂️ @' + uye.id.split('@')[0] + '👤';
+                    mesaj += '\n 👤 @' + uye.id.split('@')[0] + 'ㅤ';
                     jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
                 }
             );
