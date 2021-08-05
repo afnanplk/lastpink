f85fa85b1eb631a7426b9de6d9e9884ab5abcbbd
Afnan PLK <83832372+afnanplk@users.noreply.github.com>
2021-08-05 15:07:07 +0530

    Update language.js

--- language.js
+++ language.js
@@ -2,6 +2,7 @@ const Config = require('./config');
 const fs = require('fs');
 const chalk = require('chalk');
 
+
 if (fs.existsSync('./language/' + Config.LANG + '.json')) {
     console.log(
         chalk.green.bold('Loading ' + Config.LANG + ' language...')
