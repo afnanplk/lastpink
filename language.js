6e84869f0016f3e325e3f934dae5581f86874c9d
Afnan PLK <83832372+afnanplk@users.noreply.github.com>
2021-08-05 15:07:30 +0530

    tag reply  added

--- language.js
+++ language.js
@@ -2,7 +2,6 @@ const Config = require('./config');
 const fs = require('fs');
 const chalk = require('chalk');
 
-
 if (fs.existsSync('./language/' + Config.LANG + '.json')) {
     console.log(
         chalk.green.bold('Loading ' + Config.LANG + ' language...')
