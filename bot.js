a11dcf6a622a1671822792e164a4b0e19b37ecd8
saidali <86299543+saidalisaid2@users.noreply.github.com>
2021-08-07 17:19:43 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -133,7 +133,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please wait.')}`);
         });
 
         console.log(
-            chalk.green.bold('Pinky bot working perfectly 👻')
+            chalk.green.bold('ʟɪͥʏͭᴀᷤᴍᷟᴏͤʟ bot working perfectly 👻')
         );
     });
     
@@ -245,7 +245,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please wait.')}`);
                                     'Gerçekleşen Hata: ' + error + '\n\n'
                                     , MessageType.text);
                             } else {
-                                await conn.sendMessage(conn.user.jid, 'PINKYBOT_🐱_[error] ' +
+                                await conn.sendMessage(conn.user.jid, 'ʟɪͥʏͭᴀᷤᴍᷟᴏͤʟ baby _🐱_[error] ' +
                                     '\n\n*👻 ' + error + '*\n'
                                     , MessageType.text);
                             }
