62834d44e70ade509dab1ed16d160b4f28cb9780
saidali <86299543+saidalisaid2@users.noreply.github.com>
2021-08-07 17:49:43 +0530

    Update clear.js

--- plugins/clear.js
+++ plugins/clear.js
@@ -25,5 +25,5 @@ Asena.addCommand({pattern: 'clear', fromMe: true, desc: END, dontAddCommandList:
 
     await message.sendMessage('```cleaning chat...```');
     await message.client.modifyChat (message.jid, ChatModification.delete);
-    await message.sendMessage('```🏳 Chat cleared 🏳```');
+    await message.sendMessage('```🏳 Chat cleared 🏳```\n```BY ʟɪͥʏͭᴀᷤᴍᷟᴏͤʟ```');
 }));
