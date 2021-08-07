d302a44b207c8076e08b362b62e2403558e66271
saidali <86299543+saidalisaid2@users.noreply.github.com>
2021-08-07 18:29:51 +0530

    Update helpers.js
    😅

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *pinky*:  ```" + msg + "```"
+    return "✅ *LiyaMol*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *pinky*:  ```" + msg + "```"
+    return "🛑 *LiyaMol*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *pinky*:  ```" + msg + "```"
+    return "⏺️ *LiyaMol*:  ```" + msg + "```"
 }
 
 
