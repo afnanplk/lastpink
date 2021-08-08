0bae52eaec29550c809d464a74b701b550483070
Afnan PLK <83832372+afnanplk@users.noreply.github.com>
2021-08-08 22:05:08 +0530

    Update bgmchanger.js

--- plugins/bgmchanger.js
+++ plugins/bgmchanger.js
@@ -15,7 +15,7 @@ let baseURI = '/apps/' + config.HEROKU.APP_NAME;
  
   if (config.LANG == 'EN') {
     
-    plk_desc = 'മറുപടി bgm മോഡ് മാറ്റാൻ'
+    plk_desc = 'change reply message mod BGM'
     BGM_ONE = '𝐁𝐆𝐌 𝐭𝐲𝐩𝐞 𝐜𝐡𝐞𝐧𝐠𝐞𝐝 𝐭𝐨 𝟏𝐬𝐭 𝐦𝐨𝐝𝐞'
     BGM_TWO = '𝐁𝐆𝐌 𝐭𝐲𝐩𝐞 𝐜𝐡𝐞𝐧𝐠𝐞𝐝 𝐭𝐨 𝟐𝐧𝐝 𝐦𝐨𝐝𝐞'
     }
