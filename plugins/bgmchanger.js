0d90f3113d9569b624145638fd3df2a828989b9f
Afnan PLK <83832372+afnanplk@users.noreply.github.com>
2021-08-09 09:54:37 +0530

    PINKY V2

--- plugins/bgmchanger.js
+++ plugins/bgmchanger.js
@@ -16,8 +16,8 @@ let baseURI = '/apps/' + config.HEROKU.APP_NAME;
   if (config.LANG == 'EN') {
     
     plk_desc = 'change reply message BGM mode'
-    BGM_ONE = '𝐁𝐆𝐌 𝐭𝐲𝐩𝐞 𝐜𝐡𝐞𝐧𝐠𝐞𝐝 𝐭𝐨 𝟏𝐬𝐭 𝐦𝐨𝐝𝐞'
-    BGM_TWO = '𝐁𝐆𝐌 𝐭𝐲𝐩𝐞 𝐜𝐡𝐞𝐧𝐠𝐞𝐝 𝐭𝐨 𝟐𝐧𝐝 𝐦𝐨𝐝𝐞'
+    BGM_ONE = '𝐁𝐆𝐌 𝐭𝐲𝐩𝐞 𝐜𝐡𝗮𝐧𝐠𝐞𝐝 𝐭𝐨 𝟭𝘀𝘁 𝐦𝐨𝐝𝐞'
+    BGM_TWO = '𝐁𝐆𝐌 𝐭𝐲𝐩𝐞 𝐜𝐡𝗮𝐧𝐠𝐞𝐝 𝐭𝐨 𝟐𝐧𝐝 𝐦𝐨𝐝𝐞'
     }
 
     if (config.LANG == 'ML') {
