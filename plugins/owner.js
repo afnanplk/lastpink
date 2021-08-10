6dac6be10eac222101edf3db91dd4f4bc9ee72cc
saidali <86299543+saidalisaid2@users.noreply.github.com>
2021-08-10 15:18:08 +0530

    Update owner.js
    Nokkanda unni nan thanne

--- plugins/owner.js
+++ plugins/owner.js
@@ -1,5 +1,5 @@
 /* Copyright (C) 2021 AFNAN PALLIKKEL
-CODDED AFNAN PALLIKKEL
+CODDED AFNAN PALLIKKEL and saidali
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 PINKY
