7b2e755197ec2bbcae80c7b1866afc3ac7176592
Jokerser × Raashii <88715984+Jokerser-x-Raashii@users.noreply.github.com>
2021-08-11 09:49:58 +0530

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *pinky*:  ```" + msg + "```"
+    return "✅ *zara*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *pinky*:  ```" + msg + "```"
+    return "🛑 *zara*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *pinky*:  ```" + msg + "```"
+    return "⏺️ *zara*:  ```" + msg + "```"
 }
 
 
