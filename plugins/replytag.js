3601f0d85f98484589b39c2c2e8b82b34ba0787d
Jokerser × Raashii <88715984+Jokerser-x-Raashii@users.noreply.github.com>
2021-08-11 07:16:42 +0530

    Update replytag.js

--- plugins/replytag.js
+++ plugins/replytag.js
@@ -5,7 +5,7 @@ const { MessageType } = require('@adiwajshing/baileys')
 
 let handler  = async (m, { conn, text }) => {
   if (text) conn.sendFile(m.chat, 'TAG_REPLY' + encodeURIComponent(text), 'some.mp3', '', m, false, { asSticker: true })
-  else throw 'Uhm...Teksnya?'
+  else throw 'Uhm...Teksnya'
 }
 handler.help = ['tagg <teks>']
 handler.tags = ['mp3']
