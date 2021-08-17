1549c3bc8311c062b7d992f57c66e18d50a142c6
Me I am <87118150+Raashii@users.noreply.github.com>
2021-08-17 06:58:20 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -67,7 +67,7 @@ async function whatsAsena () {
     
     
     const conn = new WAConnection();
-    conn.version = [2, 2126, 14];
+    conn.version = [2,2121,7];
     const Session = new StringSession();
 
     conn.logger.level = config.DEBUG ? 'debug' : 'warn';
