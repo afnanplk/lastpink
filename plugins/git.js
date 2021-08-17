a40a1f989308bc7743acee902785e6e151509d8b
Me I am <87118150+Raashii@users.noreply.github.com>
2021-08-17 10:29:45 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -25,17 +25,17 @@ if (Config.WORKTYPE == 'private') {
         }
 
         if (Config.PLK == 'default') {
-            await message.client.sendMessage(message.jid,'𝐙𝐚𝐫𝐚𝐌𝐰𝐨𝐥 Bot created by *ʀᴀᴀsʜɪɪ & ᴊᴏᴋᴇʀsᴇʀ*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'𝐙𝐚𝐫𝐚𝐌𝐰𝐨𝐥 Bot created by *ʀᴀᴀsʜɪɪ*' , MessageType.text);
         }
         else {
-            await message.client.sendMessage(message.jid,Config.PLK + '\n\n*GIT LINK* - ```https://github.com/Jokerser-x-Raashii/Zaramwol```', MessageType.text);
+            await message.client.sendMessage(message.jid,Config.PLK + '\n\n*🖤GIT LINK* - ```https://github.com/Jokerser-x-Raashii/Zaramwol```', MessageType.text);
         }
     }));
 }
 
 else if (Config.WORKTYPE == 'public') {
 
-    Asena.addCommand({pattern: 'git', fromMe: false, desc: 'shows the detail of bot owner'}, (async (message, match) => {
+    Asena.addCommand({pattern: 'git', fromMe: false, desc: 'shows the git link of bot'}, (async (message, match) => {
 
         if (message.jid === '54218542512-1612300121@g.us') {
 
@@ -43,10 +43,10 @@ else if (Config.WORKTYPE == 'public') {
         }
 
         if (Config.PLK == 'default') {
-            await message.client.sendMessage(message.jid,'𝐙𝐚𝐫𝐚𝐌𝐰𝐨𝐥 Bot created by *ʀᴀᴀsʜɪɪ & ᴊᴏᴋᴇʀsᴇʀ* \n\n *GIT LINK* - ```https://github.com/Jokerser-x-Raashii/Zaramwol```' , MessageType.text);
+            await message.client.sendMessage(message.jid,'𝐙𝐚𝐫𝐚𝐌𝐰𝐨𝐥 Bot created by *ʀᴀᴀsʜɪɪ* \n\n *GIT LINK* - ```https://github.com/Jokerser-x-Raashii/Zaramwol```' , MessageType.text);
         }
         else {
-            await message.client.sendMessage(message.jid,Config.PLK + '\n\n*GIT LINK* - ```https://github.com/Jokerser-x-Raashii/Zaramwol```', MessageType.text);
+            await message.client.sendMessage(message.jid,Config.PLK + '\n\n*🌚GIT LINK* - ```https://github.com/Jokerser-x-Raashii/Zaramwol```', MessageType.text);
         }
     }));
 }
