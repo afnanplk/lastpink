9e64c0f76aeba4905af70f90aaca5fa3c4abad6a
Raashii <87118150+Raashii@users.noreply.github.com>
2021-08-23 09:14:49 +0530

    Create spdf.js

---
+++ plugins/spdf.js
@@ -0,0 +1,36 @@
+/* # Exclusively from Raashii 
+ */
+
+const Raashii = require('../events');
+const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
+const fs = require('fs');
+const axios = require('axios');
+const Config = require('../config');
+const need = "*add link after command💌*"
+
+if (Config.WORKTYPE == 'private') {
+
+  Raashii.addCommand({ pattern: 'spdf ?(.*)', fromMe: true, desc: 'Get screenshot of links in pdf'}, (async (message, match) => {
+
+    if (match[1] === '') return await message.sendMessage(need);
+
+    var rashi = await axios.get(`https://api.html2pdf.app/v1/generate?url=${match[1]}&apiKey=begC4dFAup1b8LyRXxAfjetfqDg2uYx8PWmh9YJ59tTZXiUyh2Vs72HdYQB68vyc`, { responseType: 'arraybuffer' })
+    
+    await message.sendMessage(Buffer.from(rashi.data), MessageType.document, { filename: 'raashii.pdf', mimetype: Mimetype.pdf});
+
+  }));
+}
+
+else if (Config.WORKTYPE == 'public') {
+
+  Raashii.addCommand({ pattern: 'spdf ?(.*)', fromMe: false, desc: 'Get screenshot of links in pdf'}, (async (message, match) => {
+
+    if (match[1] === '') return await message.sendMessage(need);
+
+    var rashi = await axios.get(`https://api.html2pdf.app/v1/generate?url=${match[1]}&apiKey=begC4dFAup1b8LyRXxAfjetfqDg2uYx8PWmh9YJ59tTZXiUyh2Vs72HdYQB68vyc`, { responseType: 'arraybuffer' })
+
+    await message.sendMessage(Buffer.from(rashi.data), MessageType.document, {filename: 'sspdf.pdf', mimetype: Mimetype.pdf});
+
+  }));
+
+}
