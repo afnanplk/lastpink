3ab34ef18b30f40953430439e3ff340868e64e7f
Afnan PLK <83832372+afnanplk@users.noreply.github.com>
2021-08-23 23:43:44 +0530

    new work type added

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -1,7 +1,6 @@
 /* Copyright (C) 2020 Yusuf Usta.
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-WhatsAsena - Yusuf Usta
+recodded by afnanplk
+new work type by afnanplk
 */
 
 const Asena = require('../events');
