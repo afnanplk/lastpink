1a0e2ec62fe7b174b8d7bd42c649ac537a47b28e
Afnan PLK <83832372+afnanplk@users.noreply.github.com>
2021-08-24 18:53:41 +0530

    DONT UPDATE

---
+++ pinky.js
@@ -0,0 +1,17 @@
+/* codded by afnanplk
+*/
+
+const { Sequelize } = require('sequelize');
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+// PLK Special Functions
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+
+module.exports = {
+  
+// BOTNAME: process.env.AUTO === undefined ? 'Pinky' : process.env.AUTO
+
+};
