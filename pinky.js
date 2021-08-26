e7135e5a92c75e1e4e2962433daa7f164afd03e2
Afnan PLK <83832372+afnanplk@users.noreply.github.com>
2021-08-26 20:41:51 +0530

    Update pinky.js

--- pinky.js
+++ pinky.js
@@ -11,7 +11,11 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-  
-// BOTNAME: process.env.AUTO === undefined ? 'Pinky' : process.env.AUTO
+
+ OA_AI: process.env.Z_PINKY_AI === undefined ? true : process.env.Z_BOTNAME,  
+ OB_NAME: process.env.Z_BOT_NAME === undefined ? 'Pinky' : process.env.Z_BOT_NAME,
+ OA_NAME: process.env.Z_DEP_NAME === undefined ? 'plk' : process.env.Z_DEP_NAME,
+ OA_REPLY: process.env.Z_DEP_REPLY === undefined ? 'plk is my creator his full name is afnanplk' : process.env.Z_DEP_REPLY,
+    
 
 };
