3e74167fc97b0aae818a1246d5e534964c6330ee
Raashii <87118150+Raashii@users.noreply.github.com>
2021-08-28 19:52:26 +0530

    Update adan.js

--- plugins/adan.js
+++ plugins/adan.js
@@ -1,5 +1,6 @@
 /* Codded by @mrclfd // rapiUp
-PINKY V2 :)
+Telegram: t.me/mrclfd
+Semoga Berfaedah dan Berkah :)
 */
 
 const Asena = require('../events');
@@ -12,11 +13,10 @@ const adzan = "Finds prayer time."
 const usage = ".prayer <city>"
 
 const butuh = "```give a city name!```"
-const renek = "```Not found the city! 🥴```"
+const renek = "```Not found the city!```"
 
-if (Config.WORKTYPE == 'private') {
 
-        Asena.addCommand({pattern: 'adan ?(.*)', desc: adzan, usage: usage, fromMe: true}, async (message, match) => {
+        Asena.addCommand({pattern: 'prayer ?(.*)', desc: adzan, usage: usage, fromMe: false}, async (message, match) => {
 
 	    if (match[1] === '') return await message.reply(butuh);
 	    const url = `https://api.pray.zone/v2/times/today.json?city=${match[1]}`;
@@ -31,33 +31,8 @@ if (Config.WORKTYPE == 'private') {
 		    '```Dzuhur  :``` *' + json.results.datetime[0].times.Dhuhr + '*\n' + 
 		    '```Asar    :``` *' + json.results.datetime[0].times.Asr + '*\n' + 
 		    '```Maghrib :``` *' + json.results.datetime[0].times.Maghrib + '*\n' +
-        '```Isha    :``` *' + json.results.datetime[0].times.Isha + '*\n', MessageType.text);
+                    '```Isya    :``` *' + json.results.datetime[0].times.Isha + '*\n', MessageType.text);
 	    } catch {
 		    return await message.client.sendMessage(message.jid, renek, MessageType.text);
 	    }
     });
-}
-
-else if (Config.WORKTYPE == 'public') {
-
-        Asena.addCommand({pattern: 'adan ?(.*)', desc: adzan, usage: usage, fromMe: false}, async (message, match) => {
-
-	    if (match[1] === '') return await message.reply(butuh);
-	    const url = `https://api.pray.zone/v2/times/today.json?city=${match[1]}`;
-	    try {
-		    const response = await got(url);
-		    const json = JSON.parse(response.body);
-		    if (response.statusCode === 200) return await message.client.sendMessage(message.jid,  '*Jadwal Sholat*:\n' +
-                    '📅 ```' + json.results.datetime[0].date.gregorian + ' | ' + json.results.location.timezone + '```\n' +
-                    '🌏 ```' + json.results.location.city + ' | ' + json.results.location.country + '```\n\n' +
-		    '```Imsak   :``` *' + json.results.datetime[0].times.Imsak + '*\n' + 
-		    '```Subuh   :``` *' + json.results.datetime[0].times.Fajr + '*\n' +
-		    '```Dzuhur  :``` *' + json.results.datetime[0].times.Dhuhr + '*\n' + 
-		    '```Asar    :``` *' + json.results.datetime[0].times.Asr + '*\n' + 
-		    '```Maghrib :``` *' + json.results.datetime[0].times.Maghrib + '*\n' +
-        '```Isha    :``` *' + json.results.datetime[0].times.Isha + '*\n', MessageType.text);
-	    } catch {
-		    return await message.client.sendMessage(message.jid, renek, MessageType.text, quoted: message.data});
-	    }
-    });
-}
