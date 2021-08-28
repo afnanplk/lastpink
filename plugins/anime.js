598a844c74c61165cde6a40d48dcc278393d481b
Raashii <87118150+Raashii@users.noreply.github.com>
2021-08-28 12:33:28 +0530

    ZᴀʀͥᴀͭᴍᷤWᷟᴏͤʟ

--- plugins/anime.js
+++ plugins/anime.js
@@ -190,7 +190,7 @@ else if (Config.WORKTYPE == 'public') {
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {quoted: message.data , mimetype: Mimetype.jpg, contextInfo: { forwardingScore: 1000, isForwarded: true}, caption: Config.AFN})
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {quoted: message.data , mimetype: Mimetype.jpg, caption: Config.AFN})
 
     }));
 }
