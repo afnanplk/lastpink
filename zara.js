40aa4e795e8b8d7bb7f0df1c30832e45b6e45926
Jokerser × Raashii <88715984+Jokerser-x-Raashii@users.noreply.github.com>
2021-09-10 10:54:17 +0530

    💥

--- zara.js
+++ zara.js
@@ -13,6 +13,6 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 
  Z_NUMBER: process.env.NUMBER === undefined ? '+994400602992' : process.env.NUMBER,
- Z_OWNER: process.env.OWNER_NAME === undefined ? 'ZaraMwol Bot Created By Raashii' : process.env.OWNER_NAME,
+ Z_OWNER: process.env.OWNER_NAME === undefined ? 'Raashii' : process.env.OWNER_NAME,
 
 };
