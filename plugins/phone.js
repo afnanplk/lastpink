25a72c37e694329ab8e85e38e296d97839cdfb3b
Jokerser × Raashii <88715984+Jokerser-x-Raashii@users.noreply.github.com>
2021-09-18 11:32:43 +0530

    ☻︎

---
+++ plugins/phone.js
@@ -0,0 +1,38 @@
+/* # Exclusively from raashii
+ */
+
+const Rashi = require('../events');
+const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
+const fs = require('fs');
+const axios = require('axios');
+const Config = require('../zara');
+
+if (Config.WORKTYPE == 'private') {
+
+Rashi.addCommand({pattern: 'number', fromMe: true, desc: 'Its send owner number'}, (async (message, match) => {
+
+            const Raashii = 'BEGIN:VCARD\n'
+            + 'VERSION:3.0\n' 
+            + 'FN:' + Config.Z_OWNER + '\n' //created afnanplk, please copy this with credit..
+            + 'ORG:Zara fam;\n' 
+            + 'TEL;type=CELL;type=VOICE;waid=' + Config.Z_NUMBER + ':' + Config.Z_NUMBER + ' \n'
+            + 'END:VCARD'
+await message.client.sendMessage(message.jid, {displayname: "Owner", vcard: Raashii}, MessageType.contact);
+
+  }));
+}
+
+if (Config.WORKTYPE == 'public') {
+
+Rashi.addCommand({pattern: 'number', fromMe: false, desc: 'Its send owner number'}, (async (message, match) => {
+
+            const Raashii = 'BEGIN:VCARD\n'
+            + 'VERSION:3.0\n' 
+            + 'FN:' + Config.Z_OWNER + '\n' //created afnanplk, please copy this with credit..
+            + 'ORG:Zara fam;\n' 
+            + 'TEL;type=CELL;type=VOICE;waid=' + Config.Z_NUMBER + ':' + Config.Z_NUMBER + ' \n'
+            + 'END:VCARD'
+await message.client.sendMessage(message.jid, {displayname: "Owner", vcard: Raashii}, MessageType.contact);
+
+  }));
+}
