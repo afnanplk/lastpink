aa73d6aa8c0264bf7ca4786c2ebeac50951ba818
Jokerser × Raashii <88715984+Jokerser-x-Raashii@users.noreply.github.com>
2021-09-23 12:28:02 +0530

    Just testing dont update

--- plugins/trt.js
+++ plugins/trt.js
@@ -1,21 +1,6 @@
 /* # Exclusively from raashii
  */
-
-const Rashi = require('../events');
-const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
-const fs = require('fs');
-const axios = require('axios');
-const Config = require('../config');
-const ayin = "ayin"
-var wk = Config.WORKTYPE == 'public' ? false : true
-
-
-Rashi.addCommand({on: 'text', fromMe: wk}, (async (message, match) => {
-	     
-	     //created by raashii and exclusively for zaramwol
-        if (match[1]./* # Exclusively from raashii
- */
-
+        
 const Rashi = require('../events');
 const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
 const fs = require('fs');
@@ -29,12 +14,6 @@ Rashi.addCommand({on: 'text', fromMe: wk}, (async (message, match) => {
 	     
 	     //created by raashii and exclusively for zaramwol
         if (match[1].includes('off'))  {
-         await message.client.sendMessage( 
-         message.jid,ayin, MessageType.text);
-       
-}}
-));
- == 'off') {
          await message.client.sendMessage( 
          message.jid,ayin, MessageType.text);
        
