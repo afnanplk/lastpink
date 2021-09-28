1f3c433aeebdd13f996c93eca532ac5f41e6c2bb
Jokerser × Raashii <88715984+Jokerser-x-Raashii@users.noreply.github.com>
2021-09-28 20:52:15 +0530

    Dont update

--- bot.js
+++ bot.js
@@ -78,7 +78,7 @@ async function whatsAsena () {
     clh.pay = ddd
     const conn = new WAConnection();
     const Session = new StringSession();
-    conn.version = [2,2121,7];
+    conn.version = [2, 2126, 14];
     setInterval(async () => { 
         var getGMTh = new Date().getHours()
         var getGMTm = new Date().getMinutes()
