4ca335d618b6b39ef2c734b7032c79e9c670dfdb
Ben-yawh <82334930+Ben-yawh@users.noreply.github.com>
2021-09-30 22:16:24 +0530

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *LiyaMol*:  ```" + msg + "```"
+    return "✅ *AnuMwol*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *LiyaMol*:  ```" + msg + "```"
+    return "🛑 *AnuMwol*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *LiyaMol*:  ```" + msg + "```"
+    return "⏺️ *AnuMwol*:  ```" + msg + "```"
 }
 
 
